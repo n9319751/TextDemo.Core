799cd6216ddbf1624d7b42c9849d49048394f38e
Roslin Punnoose <roslinpunnoose@gmail.com>
2016-09-02 17:35:57 -0700

    Scanmenu

---
+++ TestDemo.Droid/Views/ScanmenuView.cs
@@ -0,0 +1,20 @@
+using System;
+using Android.App;
+using Android.OS;
+using MvvmCross.Droid.Views;
+
+namespace ScreenshotDemo.Droid.Views
+{
+    [Activity(Label = "View for ScanmenuViewModel")]
+    public class FirstView : MvxActivity
+    {
+        
+	
+
+		protected override void OnCreate(Bundle bundle)
+		  {
+		    base.OnCreate(bundle);
+	       SetContentView(Resource.Layout.ScanmenuView);
+		   }
+	}
+}
---
+++ TextDemo.Core/ViewModels/ScanmenuViewModel.cs
@@ -0,0 +1,25 @@
+using MvvmCross.Core.ViewModels;
+
+namespace ScreenshotDemo.Core.ViewModels
+{
+	public class ScanmenuViewModel
+		: MvxViewModel
+	{
+		private string _name =" Scan Menu ";
+		public string Name
+		{
+			get { return _name; }
+			set
+			{
+				if (value != null && value != _name)
+				{
+					_name = value;
+					RaisePropertyChanged(() => Name);
+
+				}
+			
+			}
+		}
+	}
+
+}
